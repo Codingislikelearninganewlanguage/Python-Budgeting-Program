0f234c759462ccf52b0dfcf22a215f67a75aee90
Ahmet <121121504+Codingislikelearninganewlanguage@users.noreply.github.com>
2022-12-24 17:45:41 +0000

    Create Budget.py

---
+++ Budget.py
@@ -0,0 +1,10 @@
+salary = input("Salary?")
+rent = input("Rent?")
+ulti = input("Ultites?")
+groc = input("Groocies?")
+salaryp = int(salary)
+rentp = int(rent)
+ultip = int(ulti)
+grocp = int(groc)
+expenses = rentp+ultip+grocp
+print(salaryp - expenses)
